46c2a63a31b7fbac6a3fde92892a728ce31191e4
Jason Mitcheson <jmitcheson@gmail.com>
2012-10-18 21:41:13 +1100

    test

--- www/index.js
+++ www/index.js
@@ -1,16 +1,109 @@
+var appjet = (function(){
+	
+	return {
+		
+		onLoadStart: function() {},
+		onLoadEnd: function() {},
+		safelyLoadStart: function() {
+			try {
+				onLoadStart();
+			} catch (e) {
+				
+			}
+		},
+		safelyLoadEnd: function() {
+			try {
+				onLoadEnd();
+			} catch (e) {
+				
+			}
+		},
+		init: function() {
+			var appjet = this;
+			if(window.console) {
+				console.log("Initialising script executor");
+			}
+			var domain;
+			var endpoint;
+
+			resolveEndpoints();
+			
+			$.fn.extend({
+				completeFunction: function() {},
+				complete: function(completeFunction){
+					this.completeFunction = completeFunction;
+				},
+				server:function(data, serverFunction) {
+					var rhino = this;
+					var theFunction;
+					if(typeof arguments[0] == 'function') {
+						// No data, only the callback function
+						theFunction = arguments[0];
+						var script = theFunction.toString();
+						appjet.safelyLoadStart();
+						$.post(endpoint, {script:script}, function(data){
+							try {
+								rhino.completeFunction(data);
+							} finally {
+								appjet.safelyLoadEnd();
+							}
+						});
+					} else {
+						// Both data and callback function
+						var script = serverFunction.toString();
+						var dataString = JSON.stringify(data);
+						appjet.safelyLoadStart();
+						console.log("sending query to " + endpoint);
+						$.post(endpoint, {script:script, data:dataString}, function(data){
+							alert('ajax call');
+							try {
+								rhino.completeFunction(data);
+							} finally {
+								appjet.safelyLoadEnd();
+							}
+						}).error(function(jqXHR, textStatus, errorThrown){
+							window.alert('error making ajax call to backend' + textStatus + errorThrown);
+						});
+					}
+					return this;
+				}
+			});
+			
+			function resolveDomain() {
+				if(window.PhoneGap) {
+					domain = "http://www.pixeljet.net/";
+				} else {
+					var host = window.location.hostname;
+					domain = "http://" + host + "/";
+				}
+			}
+			
+			function resolveEndpoints() {
+				resolveDomain();
+				endpoint = domain + "script/execute";
+			}
+		}
+	}
+	
+	
+})();
+
+appjet.init();
+
+
 document.addEventListener("deviceready", function(){
     
      console.log("Executing document ready.");
      window.alert('ready');
      appjet.onLoadStart = function() {
-    	 //$("#go").button('loading');
+    	 $("#go").button('loading');
      };
      appjet.onLoadEnd = function() {
-    	// $("#go").button('reset');
+    	 $("#go").button('reset');
      };
      
      $("#go").click(function(){
-        window.alert('go');
+         window.alert('go');
     	// If deploying this for real, you will want some better input validation! This only checks string length
         var query = $("#query").val();
         if(query.length<1) {
@@ -18,12 +111,11 @@ document.addEventListener("deviceready", function(){
         }
         
         var data = { query : query };
-        console.log("Requesting product data.");
+        console.log("Requesting product data");
         
 		$().server(data, function(data){
 			if(validate(data)) {
-				var a = shopping.queryProducts(data.query);
-				return a.items;
+				return shopping.queryProducts(data.query).items;
 			} else {
 				return [{}];
 			}
@@ -31,7 +123,7 @@ document.addEventListener("deviceready", function(){
 				return (data && data.query && data.query != '' && data.query.length<20);
 			}
     	}).complete(function(result) {
-            console.log("Product data received.");
+    		window.alert("Inserting " + result.length + " products");
             $(".products").empty();
             $(result).each(function(index, product){
                 insert(product);
