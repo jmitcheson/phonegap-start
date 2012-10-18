cca1de350a91a00b3a7c53c430217f307c815ab6
Jason Mitcheson <jmitcheson@gmail.com>
2012-10-18 17:09:36 +1100

    test

--- www/js/index.js
+++ www/js/index.js
@@ -1,42 +1,65 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    initialize: function() {
-        this.bind();
-    },
-    bind: function() {
-        document.addEventListener('deviceready', this.deviceready, false);
-    },
-    deviceready: function() {
-        // This is an event handler function, which means the scope is the event.
-        // So, we must explicitly called `app.report()` instead of `this.report()`.
-        app.report('deviceready');
-    },
-    report: function(id) {
-        // Report the event in the console
-        console.log("Report: " + id);
+document.addEventListener("deviceready", function(){
+    
+     console.log("Executing document ready.");
+     window.alert('ready');
+     appjet.onLoadStart = function() {
+    	 $("#go").button('loading');
+     };
+     appjet.onLoadEnd = function() {
+    	 $("#go").button('reset');
+     };
+     
+     $("#go").click(function(){
+         window.alert('go');
+    	// If deploying this for real, you will want some better input validation! This only checks string length
+        var query = $("#query").val();
+        if(query.length<1) {
+            return;
+        }
+        
+        var data = { query : query };
+        console.log("Requesting product data.", data);
+        
+		$().server(data, function(data){
+			if(validate(data)) {
+				var a = shopping.queryProducts(data.query);
+				return a.items;
+			} else {
+				return [{}];
+			}
+			function validate(data) {
+				return (data && data.query && data.query != '' && data.query.length<20);
+			}
+    	}).complete(function(result) {
+            console.log("Product data received.", result);
+            $(".products").empty();
+            $(result).each(function(index, product){
+                insert(product);
+            });
+    		
+    	});
+		
+	});
+	function insert(data) {
+        console.log("Inserting product data.");
+		/**
+		 * Many people would use a templating system here, but the HTML is very simple so
+		 * I decided to just go 'commando' with dom methods. I'm
+		 * not suggesting that people build complex UI elements this way!
+		 */
+	    var title = document.createElement('h2');
+	    var link = document.createElement('a');
+	    link.href = data.product.link;
+	    link.innerText = data.product.title;
+	    title.appendChild(link);
+		var image = document.createElement('img');
+		var container = document.createElement('section')
+		container.className = "product";
+		image.alt = "product image";
+		image.src = data.product.images[0].link;
+		container.appendChild(title);
+		container.appendChild(image);
+		$(".products").append(container);
 
-        // Toggle the state from "pending" to "complete" for the reported ID.
-        // Accomplished by adding .hide to the pending element and removing
-        // .hide from the complete element.
-        document.querySelector('#' + id + ' .pending').className += ' hide';
-        var completeElem = document.querySelector('#' + id + ' .complete');
-        completeElem.className = completeElem.className.split('hide').join('');
-    }
-};
+	}
+}, false);
